c86bc67f255735063b0124964442f6bb79bbcb3c
pipapaparapo2024 <bedzanansergej5110@gmail.com>
2025-07-19 21:21:40 +0300

    depp

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,16 @@
-import { type FC } from "react";
+import { useEffect, type FC } from "react";
 import "./App.css";
-// const tg= window.Telegram.WebApp;
+const tg = window.Telegram.WebApp;
 export const App: FC = () => {
-  // useEffect(()=>{
-  //   tg.ready();
-  // },[])
-  // const close=()=>{
-  //   tg.close()
-  // }
+  useEffect(() => {
+    tg.ready();
+  }, []);
+  const close = () => {
+    tg.close();
+  };
   return (
     <>
-      if (import.meta.env.MODE === 'production' && !window.Telegram?.WebApp){" "}
-      {console.error("Telegram WebApp API не загружен!")}
-      <button>закрыть</button>
+      <button onClick={() => close()}>закрыть</button>
       <div className="f">sdfsgdsgfdsfhb</div>
     </>
   );
